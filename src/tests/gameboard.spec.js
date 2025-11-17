03b94f8faea6e5f3da164623037bcc04c628ee1b
Tommieboy99 <tomkoek99@hotmail.com>
2025-11-17 12:53:06 +0100

    Wrote tests for the placeShip logic

--- src/tests/gameboard.spec.js
+++ src/tests/gameboard.spec.js
@@ -1,4 +1,5 @@
 import { Gameboard } from '../gameboard';
+import { Ship } from '../ship';
 
 describe('Gameboard', () => {
   let board;
@@ -27,5 +28,44 @@ describe('Gameboard', () => {
         expect(board.grid[i][j]).toBe('');
       }
     }
+  });
+
+  //Gameboards should be able to place ships at specific coordinates by calling the ship factory or class..
+
+  test('placeShip of length 1 at (x, y) marks the cell as occupied', () => {
+    board.placeShip(2, 3, 1);
+    expect(board.grid[2][3]).toBeInstanceOf(Ship);
+  });
+
+  test('Cant place a ship when the cell is already occupied', () => {
+    board.placeShip(2, 3, 1);
+
+    expect(() => {
+      board.placeShip(2, 3, 1);
+    }).toThrow('Cell is already occupied');
+  });
+
+  test('placeShip of length 2 at (x, y) with a direction of left', () => {
+    board.placeShip(1, 5, 2, 'left');
+    expect(board.grid[1][5]).toBeInstanceOf(Ship);
+    expect(board.grid[1][4]).toBeInstanceOf(Ship);
+  });
+
+  test('placeShip of length 2 at (x, y) with a direction of right', () => {
+    board.placeShip(1, 5, 2, 'right');
+    expect(board.grid[1][5]).toBeInstanceOf(Ship);
+    expect(board.grid[1][6]).toBeInstanceOf(Ship);
+  });
+
+  test('placeShip of length 2 at (x, y) with a direction of up', () => {
+    board.placeShip(1, 5, 2, 'up');
+    expect(board.grid[1][5]).toBeInstanceOf(Ship);
+    expect(board.grid[0][5]).toBeInstanceOf(Ship);
+  });
+
+  test('placeShip of length 2 at (x, y) with a direction of down', () => {
+    board.placeShip(1, 5, 2, 'down');
+    expect(board.grid[1][5]).toBeInstanceOf(Ship);
+    expect(board.grid[2][5]).toBeInstanceOf(Ship);
   });
 });
