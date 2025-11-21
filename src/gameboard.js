2fb8ee2985b14a88689a7904a90046e6da82fac1
Tommieboy99 <tomkoek99@hotmail.com>
2025-11-21 13:29:43 +0100

    placeShip method veranderd, veel duidelijker en makkelijker te begrijpen. hasShipAt() toegevoegd voor de tests en receiveAttack ook duidelijker en cleaner gemaakt

--- src/gameboard.js
+++ src/gameboard.js
@@ -14,100 +14,82 @@ export class Gameboard {
     );
   }
 
-  placeShip(x, y, length, direction) {
-    const ship = new Ship(length);
-
-    switch (direction) {
-      case 'left':
-        for (let col = y; col > y - length; col--) {
-          if (col < 0) throw new Error('Ship placement out of bounds');
-
-          if (this.grid[x][col] instanceof Ship) {
-            throw new Error('Cell is already occupied');
-          }
-        }
-
-        for (let col = y; col > y - length; col--) {
-          this.grid[x][col] = ship;
-        }
-
-        this.ships.push(ship);
-
-        break;
-
-      case 'right':
-        for (let col = y; col < y + length; col++) {
-          if (col > 9) throw new Error('Ship placement out of bounds');
-
-          if (this.grid[x][col] instanceof Ship) {
-            throw new Error('Cell is already occupied');
-          }
-        }
-
-        for (let col = y; col < y + length; col++) {
-          this.grid[x][col] = ship;
-        }
-
-        this.ships.push(ship);
-
-        break;
-
-      case 'up':
-        for (let row = x; row > x - length; row--) {
-          if (row < 0) throw new Error('Ship placement out of bounds');
-
-          if (this.grid[row][y] instanceof Ship) {
-            throw new Error('Cell is already occupied');
-          }
-        }
-
-        for (let row = x; row > x - length; row--) {
-          this.grid[row][y] = ship;
-        }
-
-        this.ships.push(ship);
-
-        break;
-
-      case 'down':
-        for (let row = x; row < x + length; row++) {
-          if (row > 9) throw new Error('Ship placement out of bounds');
-
-          if (this.grid[row][y] instanceof Ship) {
-            throw new Error('Cell is already occupied');
-          }
-        }
-
-        for (let row = x; row < x + length; row++) {
-          this.grid[row][y] = ship;
-        }
-
-        this.ships.push(ship);
-
-        break;
-
-      default:
-        throw new Error('Invalid direction');
+  placeShip(x, y, length, orientation = 'horizontal') {
+    if (x > 9 || x < 0 || y > 9 || y < 0) {
+      return { ok: false, error: 'OUT_OF_BOUNDS' };
     }
+
+    if (!length) {
+      return { ok: false, error: 'MISSING_LENGTH_ARG' };
+    }
+
+    if (orientation !== 'horizontal' && orientation !== 'vertical') {
+      return { ok: false, error: 'INVALID_ORIENTATION' };
+    }
+
+    const shipCoordinates = [];
+
+    if (orientation === 'horizontal') {
+      const placeLeft = y + length - 1 > 9;
+      const direction = placeLeft ? -1 : 1;
+      for (let i = 0; i < length; i++) {
+        shipCoordinates.push([x, y + i * direction]);
+      }
+    } else {
+      const placeUp = x + length - 1 > 9;
+      const direction = placeUp ? -1 : 1;
+      for (let i = 0; i < length; i++) {
+        shipCoordinates.push([x + i * direction, y]);
+      }
+    }
+
+    for (const [row, col] of shipCoordinates) {
+      if (this.board[row][col] instanceof Ship) {
+        return { ok: false, error: 'SHIP_OVERLAP' };
+      }
+    }
+
+    const ship = new Ship(length);
+    this.ships.push(ship);
+
+    for (const [row, col] of shipCoordinates) {
+      this.board[row][col] = ship;
+    }
+
+    return {
+      ok: true,
+      ship,
+      shipCoordinates,
+    };
+  }
+
+  hasShipAt(x, y) {
+    return this.board[x][y] instanceof Ship;
   }
 
   receiveAttack(x, y) {
-    if (this.grid[x][y] instanceof Ship) {
-      if (this.hitShots.some((coord) => coord[0] === x && coord[1] === y)) {
-        return 'Already hit';
-      } else {
-        this.hitShots.push([x, y]);
-        const ship = this.grid[x][y];
-        ship.hit();
-        return 'hit';
+    if (x < 0 || x > 9 || y < 0 || y > 9) {
+      return { hit: false, error: 'ATTACK_OUT_OF_BOUNDS' };
+    }
+
+    const allShots = this.hitShots.concat(this.missedShots);
+
+    for (const [cx, cy] of allShots) {
+      if (cx === x && cy === y) {
+        return { hit: false, error: 'ALREADY_ATTACKED_COORDINATE' };
       }
+    }
+
+    const hasHit = this.hasShipAt(x, y);
+
+    if (hasHit) {
+      const ship = this.board[x][y];
+      ship.hit();
+      this.hitShots.push([x, y]);
+      return { hit: true };
     } else {
-      if (this.missedShots.some((coord) => coord[0] === x && coord[1] === y)) {
-        return 'Already missed';
-      } else {
-        this.missedShots.push([x, y]);
-        return 'miss';
-      }
+      this.missedShots.push([x, y]);
+      return { hit: false };
     }
   }
 
